e22d6afd6dce22ff542875300c89f8692d4f683a
nimith <nimithbbz10@gmail.com>
2024-02-07 22:53:10 +0530

    class (16) : fix custom quantity on blur

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -22,14 +22,28 @@ const cartSlice = createSlice({
         state.items[id] = 1;
       }
     },
+
+    removeItemFromCart(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      delete state.items[id];
+    },
+
+    updateQuantity(
+      state,
+      action: PayloadAction<{ id: string; quantity: number }>
+    ) {
+      const { id, quantity } = action.payload;
+      state.items[id] = quantity;
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeItemFromCart, updateQuantity } =
+  cartSlice.actions;
+
 export default cartSlice.reducer;
 
 export function getNumItems(state: RootState) {
-  console.log("Hi");
   let numItems = 0;
 
   for (let id in state.cartState.items) {
@@ -42,11 +56,25 @@ export function getNumItems(state: RootState) {
 export const getMemoizedNumItems = createSelector(
   (state: RootState) => state.cartState.items,
   (items) => {
-    console.log("bye");
     let memoizedNumItems = 0;
     for (let id in items) {
       memoizedNumItems += items[id];
     }
     return memoizedNumItems;
+  }
+);
+
+export const getTotalPrice = createSelector(
+  (state: RootState) => state.cartState.items,
+
+  (state: RootState) => state.productState.products,
+
+  (items, products) => {
+    let totalPrice = 0;
+    Object.entries(items).forEach(
+      ([id, quantity]) => (totalPrice += quantity * products[id].price)
+    );
+
+    return totalPrice.toFixed(2);
   }
 );
